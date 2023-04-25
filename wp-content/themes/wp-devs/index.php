e312adde947d1760c5c48a96c5f388d8fc78d108
Nashicodes <nashi.codes@gmail.com>
2023-04-25 15:36:02 -0300

    initializing repository

---
+++ wp-content/themes/wp-devs/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html <?php language_attributes() ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset') ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body <?php body_class(); ?>>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem tempora laboriosam et perspiciatis, earum doloribus explicabo distinctio vel saepe, possimus quos quia tenetur rem repudiandae enim architecto omnis praesentium assumenda.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum cumque id, vitae nihil atque ea facere laudantium corrupti adipisci rem voluptatum, excepturi sit veritatis vero, omnis nobis maiores eveniet porro?
+
+    </p>
+    <div id="page" class="site">
+        <header>
+            <section class="top-bar">
+                <div class="logo">
+
+                </div>
+                <div class="searchbox">
+
+                </div>
+            </section>
+            <section class="menu-area">
+                <nav class="main-menu"></nav>
+            </section>
+        </header>
+        <div id="content" class="site-content">
+            <div id="primary" class="content-area">
+                <main id="main" class="site-main">
+                    <section class="hero"></section>
+                    <section class="services"></section>
+                    <section class="home-blog"></section>
+                </main>
+            </div>
+        </div>
+        <footer class="site-footer"></footer>
+    </div>
+</body>
+
+</html>
