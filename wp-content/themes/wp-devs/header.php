ff567c15f5b084119322a7fe88f1de0f4e5c273b
Nashicodes <nashi.codes@gmail.com>
2023-04-25 15:58:46 -0300

    Aula 11 finalizada

---
+++ wp-content/themes/wp-devs/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html <?php language_attributes() ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset') ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body <?php body_class(); ?>>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem tempora laboriosam et perspiciatis, earum doloribus explicabo distinctio vel saepe, possimus quos quia tenetur rem repudiandae enim architecto omnis praesentium assumenda. <br>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum cumque id, vitae nihil atque ea facere laudantium corrupti adipisci rem voluptatum, excepturi sit veritatis vero, omnis nobis maiores eveniet porro?
+    </p>
+    <div id="page" class="site">
+        <header>
+            <section class="top-bar">
+                <div class="logo">
+                    Logo
+                </div>
+                <div class="searchbox">
+                    SearchBox
+                </div>
+            </section>
+            <section class="menu-area">
+                <nav class="main-menu">
+                    Menu
+                </nav>
+            </section>
+        </header>
--- wp-content/themes/wp-devs/index.php
+++ wp-content/themes/wp-devs/index.php
@@ -1,52 +1,18 @@
-<!DOCTYPE html>
-<html <?php language_attributes() ?>>
-
-<head>
-    <meta charset="<?php bloginfo('charset') ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-
-<body <?php body_class(); ?>>
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem tempora laboriosam et perspiciatis, earum doloribus explicabo distinctio vel saepe, possimus quos quia tenetur rem repudiandae enim architecto omnis praesentium assumenda.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum cumque id, vitae nihil atque ea facere laudantium corrupti adipisci rem voluptatum, excepturi sit veritatis vero, omnis nobis maiores eveniet porro?
-
-    </p>
-    <div id="page" class="site">
-        <header>
-            <section class="top-bar">
-                <div class="logo">
-                    Logo
-                </div>
-                <div class="searchbox">
-                    SearchBox
-                </div>
+<?php get_header(); ?>
+<!--- -->
+<div id="content" class="site-content">
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main">
+            <section class="hero">
+                Hero
             </section>
-            <section class="menu-area">
-                <nav class="main-menu">
-                    Menu
-                </nav>
+            <section class="services">
+                Services</section>
+            <section class="home-blog">
+                Home-Blog
             </section>
-        </header>
-        <div id="content" class="site-content">
-            <div id="primary" class="content-area">
-                <main id="main" class="site-main">
-                    <section class="hero">
-                        Hero
-                    </section>
-                    <section class="services">
-                        Services</section>
-                    <section class="home-blog">
-                        Home-Blog
-                    </section>
-                </main>
-            </div>
-        </div>
-        <footer class="site-footer">
-            Footer
-        </footer>
+        </main>
     </div>
-</body>
-
-</html>
+</div>
+<!-- -->
+<?php get_footer(); ?>
