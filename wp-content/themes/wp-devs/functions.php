6f4d6a5727bf2c7400fae73779f4e0a3f08944fa
Nashicodes <nashi.codes@gmail.com>
2023-04-25 16:36:16 -0300

    Aula 13 finalizada

--- wp-content/themes/wp-devs/functions.php
+++ wp-content/themes/wp-devs/functions.php
@@ -2,6 +2,6 @@
 
 function wpdevs_load_scripts()
 {
-    wp_enqueue_style('wpdevs-style', get_stylesheet_uri(), array(), '1.0', 'all');
+    wp_enqueue_style('wpdevs-style', get_stylesheet_uri(), array(), filemtime(get_template_directory() . '/style.css'), 'all');
 }
-add_action('wp-enqueue-scripts','wpdevs_load_scripts');
+add_action('wp_enqueue_scripts', 'wpdevs_load_scripts');
