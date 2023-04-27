d4f0b73d44a83fc0b5a6c87218040e243a88e5b2
Nashicodes <nashi.codes@gmail.com>
2023-04-27 14:08:48 -0300

    Aula 21 Finalizada

--- wp-content/themes/wp-devs/index.php
+++ wp-content/themes/wp-devs/index.php
@@ -7,9 +7,32 @@
                 Hero
             </section>
             <section class="services">
-                Services</section>
+                Services
+            </section>
             <section class="home-blog">
-                Home-Blog
+                <div class="container">
+                    <div class="blog-items">
+
+                        <?php
+                        if (have_posts()) :
+                            while (have_posts()) : the_post();
+                        ?>
+                                <article>
+                                    <h2><?php the_title(); ?></h2>
+                                    <div class="meta-info">
+                                        <p>Posted in <?php echo get_the_date(); ?> by <?php the_author(); ?></p>
+                                        <p>Categories: <?php the_category(' '); ?></p>
+                                        <p>Tags: <?php the_tags('', ', ') ?></p>
+                                    </div>
+                                    <?php the_content(); ?>
+                                </article>
+                            <?php
+                            endwhile;
+                        else : ?>
+                            <p>Nothing yet to be displayed</p>
+                        <?php endif; ?>
+                    </div>
+                </div>
             </section>
         </main>
     </div>
