ab94cf5f3ba155fb995281bac6394836dea7e183
Nashicodes <nashi.codes@gmail.com>
2023-05-04 15:02:07 -0300

    iniciando as tags condicionais

--- archive.php
+++ archive.php
@@ -7,7 +7,6 @@
             <?php
             the_archive_title('<h1 class="archive-tittle">', '</h1>');
             the_archive_description('<div class="archive-description">', '</div>')
-
             ?>
             <div class="container">
                <div class="archive-items">
